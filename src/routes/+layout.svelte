f6153994eaa298c28775764cd99ed211649db037
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2026-03-26 10:53:23 -0400

    Format

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,9 +9,9 @@
 	import { resolve } from '$app/paths';
 	import { browser } from '$app/environment';
 	import { goto } from '$app/navigation';
-    import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
-    injectSpeedInsights();
+	injectSpeedInsights();
 
 	let { children } = $props();
 
@@ -186,7 +186,9 @@
 					</button>
 				{:else}
 					<a
-						href="{resolve('/login')}?redirect={encodeURIComponent(page.url.pathname !== resolve('/login') ? page.url.pathname : resolve('/'))}"
+						href="{resolve('/login')}?redirect={encodeURIComponent(
+							page.url.pathname !== resolve('/login') ? page.url.pathname : resolve('/')
+						)}"
 						class="cursor-pointer rounded-lg border border-stone-300 bg-white px-4 py-2 text-xs font-medium text-stone-800 transition-colors hover:bg-stone-50 dark:border-stone-600 dark:bg-stone-800 dark:text-stone-200 dark:hover:bg-stone-700/50"
 					>
 						Login
